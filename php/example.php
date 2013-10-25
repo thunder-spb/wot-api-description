5b514a92eda879decd3825441298a7c182cc81fa
www-data <www-data@www.radiorecord.ru>
2013-10-25 15:10:40 +0400

    Add PHP functions for API

---
+++ php/example.php
@@ -0,0 +1,22 @@
+<?
+include 'wot-functions.php';
+//print_r(getUser('info', '6440135', 'RU'));
+//print_r(getUser('info', '6440135,14661324', 'RU'));
+//print_r(getUser('search', 'Upgradeovec', 'RU'));
+//print_r(getUser('vehicles', '6440135', 'RU'));
+//print_r(getUser('ratings', '6440135', 'RU'));
+//print_r(getUser('stats', '6440135', 'RU', 24));
+
+//print_r(getClan('info', '40265', 'RU'));
+//print_r(getClan('search', 'Radio Record', 'RU'));
+
+//print_r(getWiki('tanks'));
+//print_r(getWiki('tankinfo', 2849));
+//print_r(getWiki('tankengines'));
+//print_r(getWiki('tankengines', 2373));
+//print_r(getWiki('tankguns'));
+//print_r(getWiki('tankradios'));
+//print_r(getWiki('tankturrets'));
+//print_r(getWiki('achievements'));
+
+?>
