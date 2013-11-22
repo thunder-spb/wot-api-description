e50f487d84f653dab99afd98b27de0c9c2591074
Satariall <milar@marsell.ws>
2013-11-22 19:00:15 +0400

    Add fields to request, change url of APIs, add cache support

--- php/example.php
+++ php/example.php
@@ -1,22 +1,27 @@
 <?php
 include 'WotClass.php';
-$wot = new Wot();
-//print_r($wot->getUser('info', '6440135', 'RU'));
-//print_r($wot->getUser('info', '6440135,14661324', 'RU'));
-//print_r($wot->getUser('search', 'Upgradeovec', 'RU'));
-//print_r($wot->getUser('vehicles', '6440135', 'RU'));
-//print_r($wot->getUser('ratings', '6440135', 'RU'));
-//print_r($wot->getUser('stats', '6440135', 'RU', 24));
-//print_r($wot->getClan('info', '40265', 'RU'));
-//print_r($wot->getClan('search', 'Radio Record', 'RU'));
-//print_r($wot->getWiki('tanks'));
-//print_r($wot->getWiki('tankinfo', 2849));
-//print_r($wot->getWiki('tankengines'));
-//print_r($wot->getWiki('tankengines', 2373));
-//print_r($wot->getWiki('tankguns'));
-//print_r($wot->getWiki('tankradios'));
-//print_r($wot->getWiki('tankturrets'));
-//print_r($wot->getWiki('achievements'));
+$wot = new Wot('RU', 'ru', '9fd2a424400aa541a994056eddeb2438');		// region, language, your <app_id_here>
+//$wot->setCacheType('memcache', $params=Array('host'=>'unix:///var/run/memcached/memcached.sock', 'port'=>0));	// You can set Memcache aggregator.
+//$wot->setCacheType('apc', $params=Array());	// Or APC, if php5-apc has been installed
 
+//$wot->setRegion('NA', '<app_id_here>');	// You may change used region anytime. Make sure - you may have specific app_id on each Region
+//$wot->setLang('es');	// And change answer language a anytime too
 
+$fields = Array(); // Make a full request only for develop. Select only usable fields in production for less request size
+
+//print_r($wot->getUser('info', '6440135', $fields));
+//print_r($wot->getUser('info', '6440135', $fields));			// Ex.: $fields = Array('accound_id', 'created_at', 'nickname', 'updated_at');
+//print_r($wot->getUser('search', 'Upgrade', $fields));			// Ex.: $fields = Array('nickname', 'id');
+//print_r($wot->getUser('vehicles', '6440135', $fields));		// Ex.: $fields = Array('mark_of_mastery', 'tank_id', 'achievements.medal_dumitru', 'statistics');
+//print_r($wot->getUser('ratings', '6440135', $fields));		// Ex.: $fields = Array('battle_avg_performance', 'battle_avg_xp.place', ... and etc. See full responce for select fields
+//print_r($wot->getClan('info', '40265', $fields));
+//print_r($wot->getClan('search', 'Radio Record', $fields));
+//print_r($wot->getWiki('tanks', null, $fields));
+//print_r($wot->getWiki('tankinfo', 2849, $fields));
+//print_r($wot->getWiki('tankengines', null, $fields));
+//print_r($wot->getWiki('tankengines', 2373, $fields));
+//print_r($wot->getWiki('tankguns', null, $fields));
+//print_r($wot->getWiki('tankradios', null, $fields));
+//print_r($wot->getWiki('tankturrets', null, $fields));
+//print_r($wot->getWiki('achievements', null, $fields));
 ?>
